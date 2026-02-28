66b3cb8ca60b58fe18426c91406843cde3cf212e
Minna <mariagorbunova2007@gmail.com>
2026-02-28 12:01:04 +0300

    Update BitUtils.py

--- 1laba/BitUtils.py
+++ 1laba/BitUtils.py
@@ -5,13 +5,10 @@ class BitUtils:
         if not bits:
             return ""
 
-        # Преобразуем каждый бит в строку и соединяем
         bits_str = ''.join(str(b) for b in bits)
 
-        # Разбиваем на группы по 4 бита
         groups = []
         for i in range(0, len(bits_str), 4):
             groups.append(bits_str[i:i + 4])
 
-        # Соединяем группы пробелами
         return ' '.join(groups)
